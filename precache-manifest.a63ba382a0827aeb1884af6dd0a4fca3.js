343b93fe809edb256bbc881822b259e5a7239e56
Roman <x0kill@ya.ru>
2018-12-25 21:04:32 +0300

    Updates

---
+++ precache-manifest.a63ba382a0827aeb1884af6dd0a4fca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61630f69d9f5b5023bcc",
+    "url": "/Project-schedule/static/css/main.cdb6dde4.chunk.css"
+  },
+  {
+    "revision": "61630f69d9f5b5023bcc",
+    "url": "/Project-schedule/static/js/main.61630f69.chunk.js"
+  },
+  {
+    "revision": "260d14e0a65734021fb6",
+    "url": "/Project-schedule/static/js/1.260d14e0.chunk.js"
+  },
+  {
+    "revision": "9aa3aee9ec5950d7268a",
+    "url": "/Project-schedule/static/js/runtime~main.9aa3aee9.js"
+  },
+  {
+    "revision": "115282c83fc033169850ff9ea90a39f6",
+    "url": "/Project-schedule/index.html"
+  }
+];
