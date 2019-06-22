cdc8551b296a914c21302be9e69261899685d116
Krasilnikov Roman <x0kill@ya.ru>
2019-06-22 19:23:41 +0300

    Update date pickers

--- src/constants/partitions.js
+++ src/constants/partitions.js
@@ -1,4 +1,4 @@
-import * as Helper from 'eventa/build/utils/dateTime'
+import { hour, minute, week, day, getMonthLength } from 'eventa/build/utils/dateTime'
 
 export const MINUTE = 'minute'
 export const HOUR = 'hour'
@@ -8,16 +8,16 @@ export const MONTH = 'month'
 export const YEAR = 'year'
 
 export const PARTITIONS_SIZES = {
-  [MINUTE]: 60000,
-  [HOUR]: 3600000,
-  [DAY]: 86400000,
-  [WEEK]: 604800000,
-  [MONTH]: date => Helper.getMonthLength(date.getFullYear(), date.getMonth()),
+  [MINUTE]: minute,
+  [HOUR]: hour,
+  [DAY]: day,
+  [WEEK]: week,
+  [MONTH]: date => day * getMonthLength(date.getFullYear(), date.getMonth()),
   [YEAR]: date => {
     const year = date.getFullYear()
     let sum = 0
     for (let i = 0; i < 12; i++) {
-      sum += Helper[DAY] * Helper.getMonthLength(year, i)
+      sum += day * getMonthLength(year, i)
     }
     return sum
   }
